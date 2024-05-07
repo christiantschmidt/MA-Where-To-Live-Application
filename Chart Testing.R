cd036225ad7a999369e7538f406afcef764b888a
Christian Schmidt <c.schmidt131@gmail.com>
2024-05-07 13:28:31 -0400

    Testing continues, need to go back and re-vamp education table

--- Chart Testing.R
+++ Chart Testing.R
@@ -34,3 +34,92 @@ plot
 SFHP %>% select(City) %>%
   distinct(City) %>%
   pull(.,City)
+# Single Family Home Price Rankings
+SFHP_Rankings <- SFHP %>%
+  filter(Year == 2023) %>%
+  select(City, `Median Price`) %>%
+  mutate(`Median Price` = rank(`Median Price`, na.last = "keep",ties.method = "max")) %>%
+  arrange(`Median Price`)
+
+cutoff_points_median_price <- SFHP_Rankings %>%
+  summarise(cutoff = quantile(`Median Price`, probs = c(0, 0.2, 0.4, 0.6, 0.8, 1), na.rm = TRUE))
+
+SFHP_Rankings <- SFHP_Rankings %>%
+  mutate(`Median Price Grade` = cut(`Median Price`, breaks = cutoff_points_median_price$cutoff, labels = c("A", "B", "C", "D", "F"), include.lowest = TRUE))
+
+# Most recent Crime Rankings 
+Crime_Rankings <- Crime %>% 
+  rename(City = Location) %>%
+  filter(Year == 2022) %>%
+  select(City, matches("Per 1K")) %>%
+  mutate_all(~replace_na(., 0)) %>%
+  mutate(across(-City, ~ rank(., na.last = "keep",ties.method = "max"))) %>%
+  arrange(`Per 1K - All Summary Offenses`)
+
+Crime %>% filter(Year == 2022) %>% arrange(`Per 1K - Criminal Homicide`) 
+
+crime_weights <- tibble(`Per 1K - All Summary Offenses` = 3,
+                        `Per 1K - Criminal Homicide` = 3,
+                        `Per 1K - Forcible Rape Total` = 3,
+                        `Per 1K - Robbery Total` = 2,           
+                        `Per 1K - Assault Total` = 2,
+                        `Per 1K - Aggravated Assault Total` = 2, 
+                        `Per 1K - Burglary Total` = 1,         
+                        `Per 1K - Larceny - Theft Total` = 1)
+weights <- as.vector(t(weights))
+Crime_Rankings <- Crime_Rankings %>% 
+  mutate(`Final Score` = rowSums(select(., -City) * weights)) %>%
+  arrange(`Final Score`)
+
+cutoff_points_crime_score <- Crime_Rankings %>%
+  summarise(cutoff = quantile(`Final Score`, probs = c(0, 0.2, 0.4, 0.6, 0.8, 1), na.rm = TRUE))
+
+# Assign grades based on cutoff points of Median Price
+Crime_Rankings <- Crime_Rankings %>%
+  mutate(`Crime Grade` = cut(`Final Score`, breaks = cutoff_points_crime_score$cutoff, labels = c("A", "B", "C", "D", "F"), include.lowest = TRUE))
+
+
+# Education Rankings for year 2023
+Education_Rankings <- Education %>%
+  filter(Year == 2023) %>%
+  select(City, `% Dropped Out`, `% Graduated`,`Student / Teacher Ratio`) %>%
+  mutate(across(-City, ~ rank(., na.last = "keep",ties.method = "max")))
+# Create a max value to reverse rankings of % Dropped Out
+max_value <- max(Education_Rankings$`% Dropped Out`, na.rm = TRUE)
+# Final Education Rankings
+Education_Rankings <- Education_Rankings %>%
+  mutate(`% Dropped Out` = max_value + 1 - `% Dropped Out`) %>%
+  arrange(`% Dropped Out`)
+
+
+
+
+# Create Weights
+crime_weights <- tibble(`Per 1K - All Summary Offenses` = 3,
+                  `Per 1K - Criminal Homicide` = 3,
+                  `Per 1K - Forcible Rape Total` = 3,
+                  `Per 1K - Robbery Total` = 2,           
+                  `Per 1K - Assault Total` = 2,
+                  `Per 1K - Aggravated Assault Total` = 2, 
+                  `Per 1K - Burglary Total` = 1,         
+                  `Per 1K - Larceny - Theft Total` = 1)
+weights <- as.vector(t(weights))
+Combined_Rankings <- SFHP_Rankings %>%
+  left_join(Education_Rankings, by = "City") %>%
+  left_join(Crime_Rankings, by = "City")
+Combined_Rankings <- Combined_Rankings %>% mutate_all(~replace_na(., 0))
+# Calculate the final score
+Combined_Rankings <- Combined_Rankings %>% 
+  mutate(`Final Score` = rowSums(select(., -City) * weights)) %>%
+  arrange(`Final Score`)
+# Convert 0 back to NA
+Combined_Rankings[Combined_Rankings == 0] <- NA
+# Create Grades
+cutoff_points <- quantile(Combined_Rankings$`Final Score`, probs = c(0, 0.2, 0.4, 0.6, 0.8, 1), na.rm = TRUE)
+# Assign grades based on cutoff points
+Combined_Rankings <- Combined_Rankings %>%
+  mutate(Grade = cut(`Final Score`, breaks = cutoff_points, labels = c("A", "B", "C", "D", "F"), include.lowest = TRUE))
+
+
+# Find bottom 5 values
+bottom_5 <- Crime %>% arrange(`Per 1K - Burglary Total`) %>% slice_head(n = 5)
--- app/app.R
+++ app/app.R
@@ -7,6 +7,7 @@ library(waiter)
 library(tidyverse)
 library(shinythemes)
 library(openxlsx)
+library(plotly)
 
 # Read Data --------------------------------------------------------------------
 Education = read.xlsx('Final Data.xlsx', sheet = 1, check.names = FALSE)
@@ -15,14 +16,90 @@ Population = read.xlsx('Final Data.xlsx', sheet = 3, check.names = FALSE)
 Weather = read.xlsx('Final Data.xlsx', sheet = 4, check.names = FALSE)
 SFHP = read.xlsx('Final Data.xlsx', sheet = 5, check.names = FALSE)
 
+# Fix weird spacing issue in files
+colnames(Education) <- gsub("\\.", " ", colnames(Education))
+colnames(Education)[2:16] <- paste0("Grade ",colnames(Education)[2:16], " Enrollment")
+colnames(Crime) <- gsub("\\.", " ", colnames(Crime))
+colnames(Population) <- gsub("\\.", " ", colnames(Population))
+colnames(Weather) <- gsub("\\.", " ", colnames(Weather))
+colnames(SFHP) <- gsub("\\.", " ", colnames(SFHP))
+# Make final changes to the data to work with dataframes and charts
+
 Cities <- SFHP %>% select(City) %>%
   distinct(City) %>%
   arrange(City) %>%
   pull(.,City)
 
+Education <- Education %>% 
+  select(City, Year = `End of School Year`, everything())
+
+Crime <- Crime %>%
+  rename(`City` = `Location`)
+
+y_axis_sfhp <- SFHP %>% colnames(.)
+y_axis_sfhp <- y_axis_sfhp[3:length(y_axis_sfhp)]
+
+y_axis_education <- Education %>% colnames(.)
+y_axis_education <- y_axis_education[3:length(y_axis_education)]
+
+y_axis_crime <- Crime %>% colnames(.)
+y_axis_crime <- y_axis_crime[3:length(y_axis_crime )]
+
+# Create Ranking Tables
+Crime_Rankings <- Crime %>% 
+  filter(Year == 2022) %>%
+  select(City, matches("Per 1K")) %>%
+  mutate(across(-City, ~ rank(., na.last = "keep",ties.method = "min"))) %>%
+  arrange(`Per 1K - All Summary Offenses`)
+
+Education_Rankings <- Education %>%
+  filter(Year == 2023) %>%
+  select(City, `% Dropped Out`, `% Graduated`,`Student / Teacher Ratio`) %>%
+  mutate(across(-City, ~ rank(., na.last = "keep",ties.method = "min")))
+
+max_value <- max(Education_Rankings$`% Dropped Out`, na.rm = TRUE)
+
+Education_Rankings <- Education_Rankings %>%
+  mutate(`% Dropped Out` = max_value + 1 - `% Dropped Out`) %>%
+  arrange(`% Dropped Out`)
+
+SFHP_Rankings <- SFHP %>%
+  filter(Year == 2023) %>%
+  select(City, `Median Price`) %>%
+  mutate(`Median Price` = rank(`Median Price`, na.last = "keep",ties.method = "min")) %>%
+  arrange(`Median Price`)
+
+Combined_Rankings <- SFHP_Rankings %>%
+  left_join(Education_Rankings, by = c("City")) %>%
+  left_join(Crime_Rankings, by = c("City"))
+
 # menuItem(text = 'Map', tabName = 'map',
 #          icon = icon('map', lib = 'font-awesome')),
 #icon = icon('equals', lib = 'font-awesome'
+# Functions --------------------------------------------------------------------
+
+compare_charts <- function(first_city, second_city, y_axis, data, dtick) {
+  filtered_data <- data %>%
+    filter(City %in% c(first_city, second_city))
+  
+  plot <- plot_ly(filtered_data,
+                  x = ~Year,
+                  y = ~get(y_axis),
+                  color = ~City,
+                  type = 'scatter',
+                  mode = 'lines+markers') %>%
+    layout(title = paste(y_axis, "Over Years"),
+           xaxis = list(title = "Year",
+                        tickmode = 'linear',
+                        dtick = dtick),
+           yaxis = list(title = y_axis),
+           showlegend = TRUE,
+           legend = list(title = "City")) %>%
+    layout(hovermode = "x unified")
+  
+  return(plot)
+}
+
 
 # UI ---------------------------------------------------------------------------
 ui <- dashboardPage(
@@ -37,7 +114,10 @@ ui <- dashboardPage(
     elevation = 4,
     sidebarMenu(
       id = 'tabs',
-      menuItem(text = 'Compare', tabName = 'compare'))),
+      menuItem(text = 'Compare', tabName = 'compare',
+               icon = icon('equals', lib = 'font-awesome')),
+      menuItem(text = 'Rankings', tabName = 'rankings',
+               icon = icon('star', lib = 'font-awesome')))),
   ## Body ----------------------------------------------------------------------
   body = dashboardBody(
     tabItems(
@@ -48,7 +128,6 @@ ui <- dashboardPage(
                     pickerInput(inputId = 'first_city',
                                 label = 'First City',
                                 choices = Cities,
-                                #selected = Cities[1],
                                 multiple = FALSE,
                                 options = list(`live-search` = TRUE,
                                                `virtual-scroll` = TRUE))),
@@ -57,19 +136,82 @@ ui <- dashboardPage(
                     pickerInput(inputId = 'second_city',
                                 label = 'Second City',
                                 choices = Cities,
-                                #selected = Cities[1],
+                                selected = Cities[2],
                                 multiple = FALSE,
                                 options = list(`live-search` = TRUE,
-                                               `virtual-scroll` = TRUE)))
-                ),
+                                               `virtual-scroll` = TRUE)))),
               fluidRow(
-                box(title = 'Charts',
+                box(title = 'Single Family Homes',
                     width = 12,
-                    )
-              )))))
+                    pickerInput(inputId = 'y_axis_sfhp',
+                                label = 'Y-axis',
+                                choices = y_axis_sfhp),
+                    htmlOutput('sfhp_text'),
+                    br(),
+                    plotlyOutput("sfhp_plot"))),
+              fluidRow(
+                box(title = 'Education',
+                    width = 12,
+                    pickerInput(inputId = 'y_axis_education',
+                                label = 'Y-axis',
+                                choices = y_axis_education),                    
+                    htmlOutput('education_text'),
+                    br(),
+                    plotlyOutput("education_plot"))),
+              fluidRow(
+                box(title = 'Crime',
+                    width = 12,
+                    pickerInput(inputId = 'y_axis_crime',
+                                label = 'Y-axis',
+                                choices = y_axis_crime),                    
+                    htmlOutput('crime_text'),
+                    br(),
+                    plotlyOutput("crime_plot")))
+              
+              ),
+      tabItem(tabName = 'rankings')
+      )))
 
 # Server -----------------------------------------------------------------------
 server <- function(input, output, session) {
+  
+  output$sfhp_plot <- renderPlotly({
+    compare_charts(input$first_city,
+                   input$second_city,
+                   input$y_axis_sfhp,
+                   SFHP,
+                   1)
+  })
+  
+  output$sfhp_text <- renderText({
+    "* Data collected from 4 articles from BostonMagazine.com.<br>
+    ** Median Price data collected from 2011, 2016, 2018-2023"
+  })
+  
+  output$education_plot <- renderPlotly({
+    compare_charts(input$first_city,
+                   input$second_city,
+                   input$y_axis_education,
+                   Education,
+                   1)
+  })
+  
+  output$education_text <- renderText({
+    "* Data collected from education datasets on mass.gov"
+  })
+  
+  output$crime_plot <- renderPlotly({
+    compare_charts(input$first_city,
+                   input$second_city,
+                   input$y_axis_crime,
+                   Crime,
+                   1)
+  })
+  
+  output$crime_text <- renderText({
+    "* Data collected from crime datasets on mass.gov"
+  })
+  
   
 }
 
